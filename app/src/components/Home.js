a03722c38972fe3b4b43316f174a18ec33277f7c
Jeremy <jeremyloyingping@googlemail.com>
2021-07-16 02:02:41 +0100

    Update Home component.

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,31 +1,31 @@
-import { Typography } from "@material-ui/core";
+import { Container, Typography } from "@material-ui/core";
 
 
 export default function Home() {
     return (
-        <Typography>
-            Hi there!
+        <Container maxWidth="sm">
+            <Typography>
+                <br />
+                Hi there!
 
-            <br />
-            <br />
+                <br /><br />
 
-            Press ‘analyse’ to get started by taking a reading of your voice.
-            <br />
-            <br />
-            Press ‘results’ to view analyses of past readings.
-            <br />
-            <br />
-            Press the cog in the top-right corner to view or update your details.
-
-
-            <br />
-            <br />
-            <br />
-            <br />
+                Press ‘analyse’ to get started by taking a reading of your voice.
+                <br /><br />
+                Press ‘results’ to view analyses of past readings.
+                <br /><br />
+                Press the cog in the top-right corner to view or update your details.
 
 
+                <br />
+                <br />
+                <br />
+                <br />
 
-            !!! Maybe here, it may be rather nice to have some information for users to read about the app and the purpose of the research (accordion FAQ style!)
-        </Typography>
+
+
+                !!! Maybe here, it may be rather nice to have some information for users to read about the app and the purpose of the research (accordion FAQ style!)
+            </Typography>
+        </Container>
     )
 }
