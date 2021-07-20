cbc502fd4bdc2a13583e1e642cd48bd7f3bb3d00
Jeremy <jeremyloyingping@googlemail.com>
2021-07-20 17:58:17 +0100

    Use craco to modify the webpack configuration for Kaldi.
    In preparation of using Kaldi (with WASM), I've had to use craco to modify the webpack config to support the worker and wasm loaders required. It seems to be a very temperamental setup, but at least it works now!

---
+++ app/craco.config.js
@@ -0,0 +1,32 @@
+
+module.exports = {
+    webpack: {
+        configure: (webpackConfig, { env, paths }) => {
+            // delete webpackConfig.plugins.ReactRefreshPlugin;
+
+            webpackConfig.output = {
+                ...webpackConfig.output,
+                filename: 'static/js/[name].bundle.js',
+            };
+
+            webpackConfig.module.rules = [
+                ...webpackConfig.module.rules,
+                {
+                    test: /.*Worker\.js$/,
+                    loader: 'worker-loader',
+                },
+                {
+                    test: /.wasm$/,
+                    type: 'javascript/auto',
+                    loader: 'file-loader',
+                },
+                /*{
+                    type: "javascript/auto",
+                    resolve: {}
+                }*/
+            ];
+
+            return webpackConfig;
+        }
+    },
+};
