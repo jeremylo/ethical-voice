a94a38c1bc48e86b3193e9c0b5410f19f8cb9c4d
Jeremy <jeremyloyingping@googlemail.com>
2021-07-21 03:28:06 +0100

    Refactor toggle button.

--- app/src/components/analysis/kaldi/toggleButton.js
+++ app/src/components/analysis/kaldi/toggleButton.js
@@ -1,97 +1,30 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-
 import Fab from '@material-ui/core/Fab';
 import { Mic, Stop } from '@material-ui/icons';
-import { makeStyles } from '@material-ui/core/styles';
+import React, { useState } from 'react';
 
-const useStyles = makeStyles((theme) => ({
-  iconStyle: {
-    marginRight: theme.spacing(1),
-  },
-}));
 
-function Icon({ onStandby }) {
-  const classes = useStyles();
+export default function ToggleButton({ disabled, onStart, onStop }) {
+  const [standby, setStandby] = useState(true);
 
-  if (onStandby) {
-    return <Mic fontSize="large" className={classes.iconStyle} />;
-  }
-
-  return <Stop fontSize="large" className={classes.iconStyle} />;
-}
-
-Icon.propTypes = {
-  onStandby: PropTypes.bool,
-};
-
-Icon.defaultProps = {
-  onStandby: true,
-};
-
-export default class ToggleButton extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.toggle = this.toggle.bind(this);
-
-    this.state = {
-      onStandby: true,
-    };
-  }
-
-  toggle() {
-    const { onStandby } = this.state;
-    if (onStandby) {
-      const { onStart } = this.props;
+  function toggle() {
+    setStandby(!standby);
+    if (standby) {
       onStart();
-      this.setState({ onStandby: false });
     } else {
-      const { onStop } = this.props;
       onStop();
-      this.setState({ onStandby: true });
     }
   }
 
-  buttonLook() {
-    const { onStandby } = this.state;
-    const look = { ButtonIcon: () => Icon(onStandby) };
-    if (onStandby) {
-      look.buttonText = 'Start';
-      look.color = 'primary';
-    } else {
-      look.buttonText = 'Stop';
-      look.color = 'secondary';
-    }
-    return look;
-  }
-
-  render() {
-    const { disabled } = this.props;
-    const { buttonText, color } = this.buttonLook();
-    const { onStandby } = this.state;
-
-    return (
-      <Fab
-        color={color}
-        disabled={disabled}
-        onClick={this.toggle}
-        size="large"
-        variant="extended"
-      >
-        <Icon onStandby={onStandby} />
-        {buttonText}
-      </Fab>
-    );
-  }
+  return (
+    <Fab
+      color={standby ? 'primary' : 'secondary'}
+      disabled={disabled}
+      onClick={toggle}
+      size="large"
+      variant="extended"
+    >
+      {standby ? <Mic fontSize="large" /> : <Stop fontSize="large" />}
+      {standby ? 'Start' : 'Stop'}
+    </Fab>
+  );
 }
-
-ToggleButton.propTypes = {
-  disabled: PropTypes.bool,
-  onStart: PropTypes.func.isRequired,
-  onStop: PropTypes.func.isRequired,
-};
-
-ToggleButton.defaultProps = {
-  disabled: false,
-};
