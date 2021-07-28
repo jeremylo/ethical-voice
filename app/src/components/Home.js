11bcb3a8b0ac40ede4ae96c518956613a591dddb
Jeremy <jeremyloyingping@googlemail.com>
2021-07-28 02:11:57 +0100

    Generalise speaking rate chart into a new rate chart component.

--- app/src/components/Home.js
+++ app/src/components/Home.js
@@ -1,6 +1,6 @@
 import { Container, makeStyles, Typography } from "@material-ui/core";
 import { useState } from "react";
-import SpeakingRateChart from "./visualisation/SpeakingRateChart";
+import RateChart from "./visualisation/RateChart";
 
 
 const useStyles = makeStyles((theme) => ({
@@ -19,6 +19,29 @@ export default function Home() {
     const classes = useStyles();
     const [zoomDomain, setZoomDomain] = useState({});
 
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+    }
+
+    const syllableRateData = [...Array(10).keys()].map(i => ({
+        x: new Date(2021, getRandomInt(0, 11), getRandomInt(0, 27)), y: getRandomInt(220, 340)
+    })).sort((a, b) => a.x - b.x);
+
+    /*const syllableRateData = [
+        { x: new Date(2021, 5, 14), y: 288 },
+        { x: new Date(2021, 5, 21), y: 228 },
+        { x: new Date(2021, 5, 28), y: 270 },
+        { x: new Date(2021, 6, 5), y: 330 },
+        { x: new Date(2021, 6, 9), y: 348 },
+        { x: new Date(2021, 6, 12), y: 294 },
+        { x: new Date(2021, 6, 17), y: 186 },
+        { x: new Date(2021, 6, 19), y: 166 },
+        { x: new Date(2021, 6, 24), y: 234 },
+        { x: new Date(2021, 6, 26), y: 222 },
+    ];*/
+
     return (
         <div className={classes.root}>
             <Container maxWidth="sm" className={classes.header}>
@@ -26,7 +49,12 @@ export default function Home() {
             </Container>
 
             <Container maxWidth="sm">
-                <SpeakingRateChart zoomDomain={zoomDomain} setZoomDomain={setZoomDomain} />
+                <RateChart
+                    title="Speaking rate (syllables per minute)"
+                    data={syllableRateData}
+                    zoomDomain={zoomDomain}
+                    setZoomDomain={setZoomDomain}
+                    showThreeStandardDeviations />
             </Container>
         </div >
     )
--- app/src/components/visualisation/RateChart.js
+++ app/src/components/visualisation/RateChart.js
@@ -1,5 +1,4 @@
 import { Paper } from '@material-ui/core';
-import React from 'react';
 import { max, sampleStandardDeviation } from 'simple-statistics';
 import { VictoryChart, VictoryLabel, VictoryLine, VictoryScatter, VictoryTheme, VictoryZoomContainer } from 'victory';
 
@@ -19,37 +18,11 @@ function shiftData(data, shift) {
     return data.map(datum => ({ x: datum.x, y: datum.y + shift }))
 }
 
-
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-}
-
-const syllableRateData = [...Array(10).keys()].map(i => ({
-    x: new Date(2021, getRandomInt(0, 11), getRandomInt(0, 27)), y: getRandomInt(220, 340)
-})).sort((a, b) => a.x - b.x);
-
-console.log(syllableRateData)
-
-/*const syllableRateData = [
-    { x: new Date(2021, 5, 14), y: 288 },
-    { x: new Date(2021, 5, 21), y: 228 },
-    { x: new Date(2021, 5, 28), y: 270 },
-    { x: new Date(2021, 6, 5), y: 330 },
-    { x: new Date(2021, 6, 9), y: 348 },
-    { x: new Date(2021, 6, 12), y: 294 },
-    { x: new Date(2021, 6, 17), y: 186 },
-    { x: new Date(2021, 6, 19), y: 166 },
-    { x: new Date(2021, 6, 24), y: 234 },
-    { x: new Date(2021, 6, 26), y: 222 },
-];*/
-
-export default function SpeakingRateChart({ zoomDomain, setZoomDomain }) {
-    const ys = syllableRateData.map(datum => datum.y);
-    const maximum = max(ys);
-    const stddev = sampleStandardDeviation(ys);
-    const cumulativeRollingAverageData = calculateCumulativeMovingAverage(syllableRateData);
+export default function RateChart({ title, data, zoomDomain, setZoomDomain, showThreeStandardDeviations }) {
+    const rates = data.map(datum => datum.y);
+    const maximum = max(rates);
+    const stddev = sampleStandardDeviation(rates);
+    const cumulativeRollingAverageData = calculateCumulativeMovingAverage(data);
 
     return (
         <Paper elevation={1} variant="outlined">
@@ -71,7 +44,7 @@ export default function SpeakingRateChart({ zoomDomain, setZoomDomain }) {
                     />
                 }>
 
-                <VictoryLabel text="Speaking rate (syllables per minute)" x={225} y={30} textAnchor="middle" />
+                <VictoryLabel text={title} x={225} y={30} textAnchor="middle" />
 
                 {/* "Ground truth" +/- std devs */}
                 <VictoryLine
@@ -86,22 +59,25 @@ export default function SpeakingRateChart({ zoomDomain, setZoomDomain }) {
                     interpolation="catmullRom" data={shiftData(cumulativeRollingAverageData, -stddev)}
                     style={{ data: { stroke: "#fed8b1" } }}
                 />
-                <VictoryLine
-                    interpolation="catmullRom" data={shiftData(cumulativeRollingAverageData, 3 * stddev)}
-                    style={{ data: { stroke: "#ffcccb" } }}
-                />
-                <VictoryLine
-                    interpolation="catmullRom" data={shiftData(cumulativeRollingAverageData, -3 * stddev)}
-                    style={{ data: { stroke: "#ffcccb" } }}
-                />
+
+                {showThreeStandardDeviations && (<>
+                    <VictoryLine
+                        interpolation="catmullRom" data={shiftData(cumulativeRollingAverageData, 3 * stddev)}
+                        style={{ data: { stroke: "#ffcccb" } }}
+                    />
+                    <VictoryLine
+                        interpolation="catmullRom" data={shiftData(cumulativeRollingAverageData, -3 * stddev)}
+                        style={{ data: { stroke: "#ffcccb" } }}
+                    />
+                </>)}
 
                 {/* Data */}
                 <VictoryLine
-                    interpolation="bundle" data={syllableRateData}
+                    interpolation="bundle" data={data}
                     style={{ data: { stroke: "#00e5ff" } }}
                 />
 
-                <VictoryScatter data={syllableRateData}
+                <VictoryScatter data={data}
                     size={5}
                     style={{ data: { fill: "#00e5ff" } }}
                 />
