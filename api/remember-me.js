5829d9992818cbefe98df4b8f3330b5929c090d5
Jeremy <jeremyloyingping@googlemail.com>
2021-08-05 05:23:23 +0100

    Create token hash function.

--- api/remember-me.js
+++ api/remember-me.js
@@ -1,4 +1,10 @@
+import { createHash } from 'crypto';
 import pool from './db.js';
+
+
+export function hashRememberMeToken(token) {
+    return createHash('sha256').update(token).digest('hex');
+}
 
 export async function findRememberMeToken(token) {
     let conn;
