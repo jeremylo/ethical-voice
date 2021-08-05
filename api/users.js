cdaae97de1ac89906355b2cbd681fb2a3235f439
Jeremy <jeremyloyingping@googlemail.com>
2021-08-05 22:41:42 +0100

    Drastically clean up database access logic.

--- api/users.js
+++ api/users.js
@@ -1,61 +1,31 @@
 import pool from './db.js';
 
-export async function findUserById(id) {
+async function query(...args) {
     let conn;
     try {
         conn = await pool.getConnection();
-        return (await conn.query("SELECT * FROM users WHERE id=? AND status=? LIMIT 1", [id, 1]))[0];
-    } catch (e) {
-        throw new Error("Unable to retrieve this user.");
+        return await conn.query(...args);
     } finally {
         conn.release();
     }
+}
+
+export async function findUserById(id) {
+    return (await query("SELECT * FROM users WHERE id=? AND status=? LIMIT 1", [id, 1]))[0];
 }
 
 export async function findUserByEmail(email) {
-    let conn;
-    try {
-        conn = await pool.getConnection();
-        return (await conn.query("SELECT * FROM users WHERE email=? AND status=? LIMIT 1", [email, 1]))[0];
-    } catch (e) {
-        throw new Error("Unable to retrieve this user.");
-    } finally {
-        conn.release();
-    }
+    return (await query("SELECT * FROM users WHERE email=? AND status=? LIMIT 1", [email, 1]))[0];
 }
 
 export async function updateUserPassword(user, password) {
-    let conn;
-    try {
-        conn = await pool.getConnection();
-        return (await conn.query("UPDATE users SET password=? WHERE id=?", [password, user.id]));
-    } catch (e) {
-        throw new Error("Unable to update the user's password.");
-    } finally {
-        conn.release();
-    }
+    return (await query("UPDATE users SET password=? WHERE id=?", [password, user.id]));
 }
 
 export async function updateUserOutwardPostcode(user, outwardPostcode) {
-    let conn;
-    try {
-        conn = await pool.getConnection();
-        return (await conn.query("UPDATE users SET outward_postcode=? WHERE id=?", [outwardPostcode, user.id]));
-    } catch (e) {
-        throw new Error("Unable to update the user's outward postcode.");
-    } finally {
-        conn.release();
-    }
+    return (await query("UPDATE users SET outward_postcode=? WHERE id=?", [outwardPostcode, user.id]));
 }
 
 export async function updateUserSharing(user, sharing) {
-    let conn;
-    try {
-        conn = await pool.getConnection();
-        return (await conn.query("UPDATE users SET sharing=? WHERE id=?", [sharing ? 1 : 0, user.id]));
-    } catch (e) {
-        throw new Error("Unable to update the user's sharing agreement status.");
-    } finally {
-        conn.release();
-    }
+    return (await query("UPDATE users SET sharing=? WHERE id=?", [sharing ? 1 : 0, user.id]));
 }
