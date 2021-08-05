ab46ac9546ecbe895d85d984353eb069a1fe7115
Jeremy <jeremyloyingping@googlemail.com>
2021-08-05 18:35:01 +0100

    Use redis for session storage.

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
+import ConnectRedis from 'connect-redis';
 import cookieParser from 'cookie-parser';
 import express from 'express';
 import session from 'express-session';
 import passport from './passport.js';
+import redisClient from './redis.js';
 import authRoutes from './routes/auth.js';
 import modelRoutes from './routes/model.js';
 import testsRoutes from './routes/tests.js';
@@ -9,10 +11,16 @@ import testsRoutes from './routes/tests.js';
 const app = express();
 const port = 4000;
 
+const RedisStore = ConnectRedis(session);
+
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
+    store: new RedisStore({
+        client: redisClient,
+        prefix: "api:session:"
+    }),
     secret: process.env.COOKIE_SECRET,
     resave: true,
     cookie: {
