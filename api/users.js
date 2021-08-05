909171c5e9de4c48805b1877c2a7da22e2d7936a
Jeremy <jeremyloyingping@googlemail.com>
2021-08-05 05:01:29 +0100

    Create a findUserByEmail function.

--- api/users.js
+++ api/users.js
@@ -1,6 +1,25 @@
-import pool from '../db.js';
+import pool from './db.js';
 
 export async function findUserById(id) {
-    const conn = await pool.getConnection();
-    return (await conn.query("SELECT * FROM users WHERE id=? LIMIT 1", [id]))[0];
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        return (await conn.query("SELECT * FROM users WHERE id=? AND status=? LIMIT 1", [id, 1]))[0];
+    } catch (e) {
+        throw new Error("Unable to retrieve this user.");
+    } finally {
+        conn.release();
+    }
+}
+
+export async function findUserByEmail(email) {
+    let conn;
+    try {
+        conn = await pool.getConnection();
+        return (await conn.query("SELECT * FROM users WHERE email=? AND status=? LIMIT 1", [email, 1]))[0];
+    } catch (e) {
+        throw new Error("Unable to retrieve this user.");
+    } finally {
+        conn.release();
+    }
 }
