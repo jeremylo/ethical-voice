b3dd62f758f87c8155b1a61784dd6abd0d375c1f
Jeremy <jeremyloyingping@googlemail.com>
2021-08-05 02:32:09 +0100

    Add SQL schema.

---
+++ mariadb/schema.sql
@@ -0,0 +1,70 @@
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+SET NAMES utf8mb4;
+
+DROP DATABASE IF EXISTS `mydata`;
+CREATE DATABASE `mydata` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `mydata`;
+
+DROP TABLE IF EXISTS `sros`;
+CREATE TABLE `sros` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `status` tinyint(3) unsigned NOT NULL DEFAULT 0,
+  `trusted` tinyint(3) unsigned NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+DROP TABLE IF EXISTS `submissions`;
+CREATE TABLE `submissions` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
+  `outward_postcode` varchar(10) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `audio` mediumblob NOT NULL,
+  `created_at` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `submissions_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+DROP TABLE IF EXISTS `submission_metadata`;
+CREATE TABLE `submission_metadata` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `submission_id` int(11) unsigned NOT NULL,
+  `metadata_key` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `metadata_value` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `submission_id` (`submission_id`),
+  CONSTRAINT `submission_metadata_ibfk_1` FOREIGN KEY (`submission_id`) REFERENCES `submissions` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `reference_id` varchar(32) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `outward_postcode` varchar(10) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `sharing` tinyint(3) unsigned NOT NULL DEFAULT 1,
+  `sro_id` int(11) unsigned NOT NULL,
+  `status` tinyint(4) NOT NULL DEFAULT 0,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `updated_at` datetime DEFAULT NULL ON UPDATE current_timestamp(),
+  PRIMARY KEY (`id`),
+  KEY `sro_id` (`sro_id`),
+  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`sro_id`) REFERENCES `sros` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+
+REVOKE ALL PRIVILEGES ON `mydata`.* FROM '7L27DLld'@'%';
+GRANT DELETE, INSERT, SELECT, UPDATE ON `mydata`.* TO '7L27DLld'@'%'
