0a52fe885c29e90bfc62668daccfbb89b3fe46fe
Jeremy <jeremyloyingping@googlemail.com>
2021-08-11 03:49:04 +0100

    Add import statement.

--- api/utils.js
+++ api/utils.js
@@ -1,4 +1,5 @@
 import bcrypt from 'bcrypt';
+import { createHash } from 'crypto';
 
 export function isValidEmail(email) {
     return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(String(email).toLowerCase());
