0fb86b3e9cf4fbe420d8d526f6627e76933a7e58
Jeremy <jeremyloyingping@googlemail.com>
2021-08-11 03:48:16 +0100

    Generate a confirmatory email modification JSON web token.

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,7 +1,8 @@
 import Router from 'express';
+import jwt from 'jsonwebtoken';
 import { updateUserOutwardPostcode, updateUserPassword, updateUserSharing } from '../persistence/users.js';
 import requireAuth from '../requireAuth.js';
-import { hashPassword, isValidEmail, isValidOutwardPostcode, isValidPasswordHash } from '../utils.js';
+import { hashPassword, hashSha256, isValidEmail, isValidOutwardPostcode, isValidPasswordHash } from '../utils.js';
 
 
 const router = Router();
@@ -24,29 +25,66 @@ router.get('/', requireAuth, async (req, res) => {
 
 });
 
+router.post('/email', requireAuth, async (req, res) => {
+    if (!req.body.email || !isValidEmail(req.body.email)) { // || req.body.email === req.user.email
+        res.status(200);
+        res.json({
+            "error": "The provided email is invalid."
+        });
+        return res;
+    }
+
+    // By signing with a SHA256 hash of the user's current email,
+    // the JWT effectively becomes "single-use" as any generated
+    // tokens are invalidated whenever the email is subsequently changed.
+    const confirmationToken = jwt.sign({
+        userid: req.user.id,
+        email: String(req.body.email).toLowerCase()
+    }, hashSha256(req.user.email), { expiresIn: '1h' });
+
+    console.log(confirmationToken);
+
+    try {
+        res.status(200);
+        res.json({
+            message: "A confirmation email has been sent."
+        });
+    } catch (e) {
+        res.status(500);
+        res.json({
+            message: "The email could not be updated successfully."
+        });
+    }
+});
+
 router.post('/password', requireAuth, async (req, res) => {
-    if (req.body.oldPassword && req.body.newPassword) {
-        if (!(await isValidPasswordHash(req.body.oldPassword, req.user.password))) {
-            res.status(401);
-            res.json({
-                "error": "The password was incorrect."
-            });
-            return res;
-        }
+    if (!req.body.oldPassword || !req.body.newPassword) {
+        res.status(401);
+        res.json({
+            "error": "Not enough information was provided to effectuate the password change."
+        });
+        return res;
+    }
 
-        try {
-            await updateUserPassword(req.user, await hashPassword(req.body.newPassword));
-            res.status(200);
-            res.json({
-                message: "The password was updated successfully."
-            });
-        } catch (e) {
-            res.status(500);
-            res.json({
-                message: "The password could not be updated successfully."
-            });
-        }
+    if (!(await isValidPasswordHash(req.body.oldPassword, req.user.password))) {
+        res.status(401);
+        res.json({
+            "error": "The password was incorrect."
+        });
+        return res;
+    }
 
+    try {
+        await updateUserPassword(req.user, await hashPassword(req.body.newPassword));
+        res.status(200);
+        res.json({
+            message: "The password was updated successfully."
+        });
+    } catch (e) {
+        res.status(500);
+        res.json({
+            message: "The password could not be updated successfully."
+        });
     }
 });
 
