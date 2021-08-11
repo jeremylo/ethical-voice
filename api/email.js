ece48bb5c3231e8ddaf756d469779e874d6fceea
Jeremy <jeremyloyingping@googlemail.com>
2021-08-11 04:31:01 +0100

    Create a crude email template.

---
+++ api/email.js
@@ -0,0 +1,28 @@
+import sgMail from '@sendgrid/mail';
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+export default function getMailer() {
+    return sgMail;
+}
+
+export function fillTemplate(body) {
+    return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Confirmation email</title>
+        <style>
+            .container {
+                font-family: "Segoe UI", "Helvetica", "Arial", sans-serif;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="container">
+            ${body}
+        </div>
+    </body>
+    </html>`;
+}
--- api/routes/email.js
+++
@@ -1,7 +0,0 @@
-import sgMail from '@sendgrid/mail';
-
-sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-
-export default function getMailer() {
-    return sgMail;
-}
