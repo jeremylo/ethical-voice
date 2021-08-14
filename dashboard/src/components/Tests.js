cf15cf9d77438fd114404de18f63f6b886ed7c1f
Jeremy <jeremyloyingping@googlemail.com>
2021-08-14 08:19:43 +0100

    Update test styling.

--- dashboard/src/components/Tests.js
+++ dashboard/src/components/Tests.js
@@ -7,16 +7,17 @@ import CustomToolbar from './patients/CustomToolbar';
 
 
 const columns = [
-    { field: 'id', headerName: 'ID', flex: 0.5, hide: true },
+    { field: 'id', headerName: 'Test ID', flex: 0.5 },
     { field: 'title', headerName: 'Title', flex: 0.8 },
-    { field: 'instruction', headerName: 'Instruction', flex: 1.5 },
+    { field: 'instruction', headerName: 'Instruction', flex: 1.5, editable: true },
     {
-        field: 'possibleDurations', headerName: 'Possible durations', flex: 1,
+        field: 'possibleDurations', headerName: 'Possible durations (sec)', flex: 1,
         valueFormatter: (params) => JSON.stringify(params.value).slice(1, -1)
     },
     {
         field: 'active', headerName: 'Enabled', flex: 0.5, type: 'boolean',
-        valueFormatter: (params) => params.value ? 1 : 0
+        valueFormatter: (params) => params.value ? 1 : 0,
+        editable: true
     },
 ];
 
@@ -37,12 +38,14 @@ export default function Tests() {
     }, [setTests]);
 
     return (<>
-        <Typography variant="h4">Speaking tests</Typography>
+        <Typography variant="h4">Speaking test types</Typography>
+        <Typography>
+            <strong>Note</strong>: in order to preserve the relationship between submissions and test definitions, test types may not be modified or deleted once created. Instead, create a new test type and disable any test types you do not wish to show patients.
+        </Typography>
         <div style={{ height: '70vh', width: '100%', marginTop: '1rem' }}>
             <DataGrid
                 rows={tests}
                 columns={columns}
-                disableSelectionOnClick
                 loading={loading}
                 components={{
                     Toolbar: CustomToolbar,
