0eba019184499a851a2e1534ab41eae229602d37
Jeremy <jeremyloyingping@googlemail.com>
2021-08-17 17:54:36 +0100

    Remove service worker from the dashboard.

--- dashboard/src/index.js
+++ dashboard/src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import { unregister as unregisterServiceWorker } from './registerServiceWorker';
 import reportWebVitals from './reportWebVitals';
+
+unregisterServiceWorker();
 
 ReactDOM.render(
   <React.StrictMode>
