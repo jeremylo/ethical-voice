9e02cadb4a15d2602f66c15a65e83f135fd37ac1
Jeremy <jeremyloyingping@googlemail.com>
2021-08-17 01:02:12 +0100

    Update export format to be an array of objects.

--- dashboard-api/routes/export.js
+++ dashboard-api/routes/export.js
@@ -1,26 +1,9 @@
 import AdmZip from 'adm-zip';
 import Router from 'express';
-import pool from '../db.js';
+import { fetchSubmissionData } from '../persistence/submissions.js';
 import requireAuth from '../requireAuth.js';
 
 const router = Router();
-
-async function fetchSubmissionData(sroId) {
-    let conn;
-    try {
-        conn = await pool.getConnection();
-        await conn.beginTransaction();
-        await conn.query("CREATE TEMPORARY TABLE export_submissions SELECT submissions.id as submission_id, users.reference_id, submissions.audio, submissions.outward_postcode, submissions.test_type_id, submissions.created_at, submissions.received_at FROM submissions, users WHERE submissions.user_id=users.id AND users.sro_id=?", [sroId]);
-        const submissions = await conn.query("SELECT * FROM export_submissions");
-        const metadata = await conn.query("SELECT submission_metadata.submission_id, submission_metadata.metadata_key, submission_metadata.metadata_value FROM submission_metadata, export_submissions WHERE submission_metadata.submission_id=export_submissions.submission_id");
-        await conn.query("DROP TABLE export_submissions");
-        await conn.commit();
-
-        return [submissions, metadata];
-    } finally {
-        if (conn) conn.release();
-    }
-}
 
 router.get('/', requireAuth, async (req, res) => {
     const submissionData = {};
@@ -37,7 +20,7 @@ router.get('/', requireAuth, async (req, res) => {
         for (const metadatum of metadata) {
             submissionData[metadatum.submission_id][metadatum.metadata_key] = metadatum.metadata_value;
         }
-        zip.addFile(`${directoryName}/submissions.json`, Buffer.from(JSON.stringify(submissionData)));
+        zip.addFile(`${directoryName}/submissions.json`, Buffer.from(JSON.stringify(Object.values(submissionData))));
 
         res
             .set('Content-Disposition', `attachment; filename="${directoryName}.zip"`)
