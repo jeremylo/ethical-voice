f0e1740984e4ef8361b1d6c0bff0940c2b82deee
Jeremy <jeremyloyingping@googlemail.com>
2021-08-17 12:22:15 +0100

    Move the registered SROs component into its own file.

---
+++ dashboard/src/components/sros/RegisteredSros.js
@@ -0,0 +1,52 @@
+import {
+    DataGrid
+} from '@material-ui/data-grid';
+import { useEffect, useState } from 'react';
+import CustomToolbar from '../patients/CustomToolbar';
+
+
+const columns = [
+    { field: 'id', headerName: 'ID', flex: 0.4 },
+    { field: 'name', headerName: 'Name', flex: 0.8 },
+    { field: 'email', headerName: 'Email', flex: 0.8 },
+    { field: 'trusted', headerName: 'Trusted', flex: 0.4, type: 'boolean' },
+    {
+        field: 'createdAt', headerName: 'Referral date', flex: 0.5,
+        valueFormatter: (params) => new Date(params.value).toLocaleString()
+    },
+    {
+        field: 'updatedAt', headerName: 'Last update', flex: 0.5,
+        valueFormatter: (params) => new Date(params.value).toLocaleString()
+    },
+];
+
+export default function RegisteredSros() {
+    const [sros, setSros] = useState([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(_ => {
+        fetch('/api/sros')
+            .then(res => res.json())
+            .then(res => {
+                if (res.sros) {
+                    setSros(res.sros);
+                }
+            })
+            .catch(console.error)
+            .finally(_ => {
+                setLoading(false);
+            });
+    }, [setSros]);
+
+    return <div style={{ height: '70vh', width: '100%', marginTop: '1rem' }}>
+        <DataGrid
+            rows={sros}
+            columns={columns}
+            disableSelectionOnClick
+            loading={loading}
+            components={{
+                Toolbar: CustomToolbar,
+            }}
+        />
+    </div>;
+}
