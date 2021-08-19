1fb7d031a2cef53d011f25dd32e1df66784a3869
Jeremy <jeremyloyingping@googlemail.com>
2021-08-19 17:06:39 +0100

    Display the word rate measure on results cards.

--- app/src/components/analysis/completion/ResultsCard.js
+++ app/src/components/analysis/completion/ResultsCard.js
@@ -23,13 +23,14 @@ export default function ResultsCard({ results, tests }) {
                         </Tooltip>
                     </IconButton>
                     : null}
-                title={`${tests[results.testId].title} (${results["speech.duration"]}s)`}
+                title={`${(tests[results.testId] ?? { title: 'Speaking test' }).title} (${results["speech.duration"]}s)`}
                 subheader={new Date(results.createdAt).toLocaleString()}
             />
             <Divider />
             <div style={{ padding: '16px' }}>
                 <Typography variant="body2" color="textSecondary" component="p">
                     <strong>Syllables per minute</strong>: {results['speech.syllablesPerMinute']} <br />
+                    <strong>Words per minute</strong>: {results['speech.wordsPerMinute']} <br />
                     {results.sputum && <><strong>Sputum colour</strong>: {sputumColours.find(x => x.value === results.sputum).name.toLowerCase()}<br /></>}
                     {results.wellbeing && <><strong>Wellbeing</strong>: {results.wellbeing} / 10<br /></>}
                     {results.dyspnoea && <><strong>MRC dyspnoea score</strong>: {results.dyspnoea}<br /></>}
