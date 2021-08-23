390ac05eadaf1a296175df3ebad12a172361c70c
Jeremy <jeremyloyingping@googlemail.com>
2021-08-23 04:39:48 +0100

    Update AboutThisAppDialog.js

--- app/src/components/settings/AboutThisAppDialog.js
+++ app/src/components/settings/AboutThisAppDialog.js
@@ -1,4 +1,4 @@
-import { DialogContentText, makeStyles } from '@material-ui/core';
+import { DialogContentText, Link, makeStyles } from '@material-ui/core';
 import Button from '@material-ui/core/Button';
 import Dialog from '@material-ui/core/Dialog';
 import DialogActions from '@material-ui/core/DialogActions';
@@ -32,9 +32,9 @@ export default function AboutThisApp(props) {
             <DialogTitle id="about-dialog-title">About this app</DialogTitle>
             <DialogContent dividers>
                 <DialogContentText>
-                    The purpose of this app is to allow you to both track your symptoms over time and share this data so that it may potentially contribute towards valuable research.
+                    The purpose of this app is to allow you to both track your symptoms over time and potentially share this data so that it may contribute towards breathlessness research.
                     <br /><br />
-                    This app was made by <a href="https://jezz.me/">Jeremy Lo Ying Ping</a> in 2021 as part of a summer research project at <a href="https://www.ucl.ac.uk/computer-science/">UCL Computer Science</a>.
+                    This app is open-source and available on <Link href="https://github.com/jeremylo/ethical-voice">GitHub (jeremylo/ethical-voice)</Link>.
                 </DialogContentText>
             </DialogContent>
             <DialogActions>
