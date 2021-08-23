424661dabd477eae8620e629a428a62da499535f
Jeremy <jeremyloyingping@googlemail.com>
2021-08-23 02:50:25 +0100

    Abstract speech recognition code into its own SpeechService class.

--- app/src/components/analysis/Speech.js
+++ app/src/components/analysis/Speech.js
@@ -3,9 +3,8 @@ import KeyboardArrowRightIcon from '@material-ui/icons/KeyboardArrowRight';
 import { Alert, AlertTitle } from '@material-ui/lab';
 import { Component, default as React } from 'react';
 import { syllable } from 'syllable';
-import { trackedDownload } from './kaldi/utils/downloadModel';
-import ASRHandler from './kaldi/workerWrappers/asrHandler';
-import IDBHandler from './kaldi/workerWrappers/idbHandler';
+import { trackedDownload } from '../../services/speech/kaldi/utils/downloadModel';
+import SpeechService from '../../services/speech/SpeechService';
 import Countdown from './speech/Countdown';
 import LabelledLinearProgress from './speech/LabelledLinearProgress';
 
@@ -37,126 +36,73 @@ class Speech extends Component {
         this.setResults = props.setResults;
         this.setAudio = props.setAudio;
 
+        this.speech = new SpeechService();
+
         this.startRecording = this.startRecording.bind(this);
         this.stopRecording = this.stopRecording.bind(this);
-        this.loadModel = this.loadModel.bind(this);
-        this.downloadModel = this.downloadModel.bind(this);
         this.handleResults = this.handleResults.bind(this);
-
-        this.recorder = null;
-        this.idbHandler = null;
-        this.asrHandler = null;
 
         this.state = {
             appStatus: STATUSES.UNINITIALISED,
             showNext: false,
-            downloadProgress: 0,
-            audioBlob: null
+            downloadProgress: 0
         };
     }
 
     componentDidMount() {
-        if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
-            this.setState({
-                appStatus: STATUSES.ERROR,
-                isRecordButtonDisabled: false,
-            });
-            return;
-        }
-
-        this.idbHandler = new IDBHandler();
-        this.asrHandler = new ASRHandler();
-
-        this.idbHandler.init({
-            name: 'asr_models',
-            version: 1,
-            storeInfo: {
-                name: 'models',
-                keyPath: 'language',
-            },
-        }).then(() => {
-            this.loadModel();
-        });
-    }
-
-    componentWillUnmount() {
-        if (this.idbHandler) {
-            this.idbHandler.terminate();
-            this.idbHandler = null;
-        }
-
-        if (this.asrHandler) {
-            this.asrHandler.terminate();
-            this.asrHandler = null;
-        }
-
-        this.recorder = null;
-    }
-
-    loadModel() {
-        this.setState({
-            appStatus: STATUSES.LOADING,
-            isRecordButtonDisabled: true,
-        });
-
-        this.idbHandler.get('model')
-            .catch(() => this.downloadModel())
-            .then(({ value: zip }) => new Promise((resolve, reject) => {
-                this.asrHandler.terminate()
-                    .then(() => { resolve(this.asrHandler.init('model', zip)); })
-                    .catch(reject);
-            }))
-            //.then(() => this.asrHandler.getSampleRate())
-            .then(() => {
-                this.setState({
-                    appStatus: STATUSES.STANDBY,
-                    isRecordButtonDisabled: false,
-                });
-            })
-            .catch((e) => {
-                console.error(e);
-                this.setState({
-                    appStatus: STATUSES.ERROR,
-                    isRecordButtonDisabled: true,
-                });
-            });
-    }
-
-    downloadModel() {
-        return new Promise((resolve, reject) => {
-            this.setState({ appStatus: STATUSES.DOWNLOADING });
-            trackedDownload('api/model', (value) => {
-                this.setState({ downloadProgress: value * 100 });
-            })
-                .then((zip) => {
-                    this.idbHandler.add('model', zip)
+        const downloadModel = (saveModel) => {
+            return new Promise((resolve, reject) => {
+                this.setState({ appStatus: STATUSES.DOWNLOADING });
+                trackedDownload('api/model', (value) => {
+                    this.setState({ downloadProgress: value * 100 });
+                }).then((zip) => {
+                    saveModel(zip)
                         .catch(console.error) // The model could not be saved, but we're proceeding anyway.
                         .finally(() => {
                             this.setState({ appStatus: STATUSES.LOADING });
                             resolve({ value: zip });
                         });
+                }).catch(reject);
+            });
+        };
+
+        try {
+            this.setState({
+                appStatus: STATUSES.LOADING,
+                isRecordButtonDisabled: true,
+            });
+
+            this.speech.setup(downloadModel)
+                .then(() => {
+                    this.setState({
+                        appStatus: STATUSES.STANDBY,
+                        isRecordButtonDisabled: false,
+                    });
                 })
-                .catch(reject);
-        });
+                .catch((e) => {
+                    // console.error(e);
+                    this.setState({
+                        appStatus: STATUSES.ERROR,
+                        isRecordButtonDisabled: true,
+                    });
+                });
+        } catch (e) {
+            this.setState({
+                appStatus: STATUSES.ERROR,
+                isRecordButtonDisabled: false,
+            });
+        }
+    }
+
+    componentWillUnmount() { // check
+        this.speech.terminate();
     }
 
     startRecording() {
         navigator.mediaDevices.getUserMedia({ audio: true }).then((microphone) => {
             this.setState({ appStatus: STATUSES.RECORDING, isRecordButtonDisabled: false });
-
-            this.recorder = window.RecordRTC(microphone, {
-                type: 'audio',
-                mimeType: 'audio/webm',
-                recorderType: window.StereoAudioRecorder,
-                numberOfAudioChannels: 1,
-                desiredSampRate: 16000
-            });
-
-            this.recorder.startRecording();
-
-            // remember to release the microphone on stopRecording
-            this.recorder.microphone = microphone;
-        }).catch(function (error) {
+            this.speech.startRecording(microphone);
+        }).catch((error) => {
             console.error(error);
             this.setState({ // unable to access mic ! TODO PUT A BETTER ERROR HERE
                 isRecordButtonDisabled: true,
@@ -169,29 +115,19 @@ class Speech extends Component {
         if (this.state.isRecordButtonDisabled) return;
 
         this.setState({ appStatus: STATUSES.STANDBY });
-        this.recorder.stopRecording(() => {
-            this.recorder.microphone.stop();
-            const blob = this.recorder.getBlob();
-            this.setState({ audioBlob: blob });
-            blob.arrayBuffer()
-                .then((buffer) =>
-                    this.asrHandler.process(new Int16Array(buffer)) // PCM
-                )
-                .then(({ text }) => {
-                    this.handleResults(text);
-                    return this.asrHandler.reset();
-                })
-                .catch(() => {
-                    this.setState({
-                        isRecordButtonDisabled: true,
-                        appStatus: STATUSES.ERROR,
-                    });
+
+        this.speech.stopRecording()
+            .then(this.handleResults)
+            .catch((e) => {
+                console.error(e);
+                this.setState({
+                    isRecordButtonDisabled: true,
+                    appStatus: STATUSES.ERROR,
                 });
-            this.recorder.microphone.getAudioTracks().forEach(track => track.stop());
-        });
+            });
     }
 
-    handleResults(transcription) {
+    handleResults({ audioBlob, transcription }) {
         const words = transcription.split(/\s+/).filter((w) => w.length > 0);
 
         const durationInMinutes = this.props.duration / 60;
@@ -199,7 +135,7 @@ class Speech extends Component {
         const syllables = words.map(syllable).reduce((a, b) => a + b, 0);
         const syllablesPerMinute = round2dp(syllables / durationInMinutes);
 
-        this.setAudio(this.state.audioBlob);
+        this.setAudio(audioBlob);
 
         this.setResults({
             syllableCount: syllables,
---
+++ app/src/services/speech/SpeechService.js
@@ -0,0 +1,96 @@
+import ASRHandler from './kaldi/workerWrappers/asrHandler';
+import IDBHandler from './kaldi/workerWrappers/idbHandler';
+
+export default class SpeechService {
+
+    constructor() {
+        this.setup = this.setup.bind(this);
+        this.terminate = this.terminate.bind(this);
+
+        this.startRecording = this.startRecording.bind(this);
+        this.stopRecording = this.stopRecording.bind(this);
+
+        this.recorder = null;
+        this.idbHandler = null;
+        this.asrHandler = null;
+    }
+
+    setup(downloadModel) {
+        if (!navigator.mediaDevices || !navigator.mediaDevices.getUserMedia) {
+            throw new Error("Unable to access any media devices.");
+        }
+
+        this.idbHandler = new IDBHandler();
+        this.asrHandler = new ASRHandler();
+
+        return this.idbHandler.init({
+            name: 'asr_models',
+            version: 1,
+            storeInfo: {
+                name: 'models',
+                keyPath: 'language',
+            },
+        })
+            .then(() => this.idbHandler.get('model'))
+            .catch(() => {
+                const saveModel = (zip) => {
+                    return this.idbHandler.add('model', zip);
+                };
+
+                return downloadModel(saveModel);
+            })
+            .then(({ value: zip }) => new Promise((resolve, reject) => {
+                this.asrHandler.terminate()
+                    .then(() => { resolve(this.asrHandler.init('model', zip)); })
+                    .catch(reject);
+            }));
+    }
+
+    startRecording(microphone) {
+        this.recorder = window.RecordRTC(microphone, {
+            type: 'audio',
+            mimeType: 'audio/webm',
+            recorderType: window.StereoAudioRecorder,
+            numberOfAudioChannels: 1,
+            desiredSampRate: 16000
+        });
+
+        this.recorder.startRecording();
+
+        // NOTE: the microphone MUST be released on stopRecording
+        this.recorder.microphone = microphone;
+    }
+
+    stopRecording() {
+        return new Promise((resolve, reject) => {
+            this.recorder.stopRecording(() => {
+                this.recorder.microphone.stop();
+                this.recorder.microphone.getAudioTracks().forEach(track => track.stop());
+
+                const audioBlob = this.recorder.getBlob();
+                audioBlob.arrayBuffer() // PCM
+                    .then((buffer) => this.asrHandler.process(new Int16Array(buffer)))
+                    .then((({ text: transcription }) => {
+                        this.asrHandler.reset();
+                        resolve({ audioBlob, transcription });
+                    }))
+                    .catch(reject);
+            });
+        });
+    }
+
+    terminate() {
+        if (this.idbHandler) {
+            this.idbHandler.terminate();
+            this.idbHandler = null;
+        }
+
+        if (this.asrHandler) {
+            this.asrHandler.terminate();
+            this.asrHandler = null;
+        }
+
+        this.recorder = null;
+    }
+
+}
