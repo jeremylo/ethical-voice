7a7e7ef4714ea721ed2c638beb19d61ec80d14c3
Jeremy <jeremyloyingping@googlemail.com>
2021-08-28 01:32:07 +0100

    Separate the ResultsCard into its own new component.

---
+++ app/src/components/ResultsCard/ResultsCard.js
@@ -0,0 +1,43 @@
+import { Card, CardHeader, Divider, IconButton, makeStyles, Tooltip, Typography } from "@material-ui/core";
+import CheckCircleIcon from '@material-ui/icons/CheckCircle';
+import MicIcon from '@material-ui/icons/Mic';
+import MicNoneIcon from '@material-ui/icons/MicNone';
+import sputumColours from "../../data/sputumColours";
+
+
+const useStyles = makeStyles((theme) => ({
+    resultsCard: {
+        marginBottom: '1rem'
+    }
+}));
+
+export default function ResultsCard({ results, tests, noAudio }) {
+    const classes = useStyles();
+
+    return (
+        <Card variant="outlined" className={classes.resultsCard}>
+            <CardHeader
+                avatar={noAudio ? <MicNoneIcon /> : <MicIcon />}
+                action={results.shared ?
+                    <IconButton>
+                        <Tooltip title="Shared">
+                            <CheckCircleIcon />
+                        </Tooltip>
+                    </IconButton>
+                    : null}
+                title={`${(tests[results.testId] ?? { title: 'Speaking test' }).title} (${results["speech.duration"]}s)`}
+                subheader={new Date(results.createdAt).toLocaleString()}
+            />
+            <Divider />
+            <div style={{ padding: '16px' }}>
+                <Typography variant="body2" color="textSecondary" component="p">
+                    <strong>Syllables per minute</strong>: {results['speech.syllablesPerMinute']} <br />
+                    {results['speech.wordsPerMinute'] && <><strong>Words per minute</strong>: {results['speech.wordsPerMinute']} <br /></>}
+                    {results.sputum && <><strong>Sputum colour</strong>: {sputumColours.find(x => x.value === results.sputum).name.toLowerCase()}<br /></>}
+                    {results.wellbeing && <><strong>Wellbeing</strong>: {results.wellbeing} / 10<br /></>}
+                    {results.dyspnoea && <><strong>MRC dyspnoea score</strong>: {results.dyspnoea}<br /></>}
+                </Typography>
+            </div>
+        </Card>
+    );
+}
